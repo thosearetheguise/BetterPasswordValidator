41e261202dfda8ce61f27b6b46bad6111494b3a4
J4C0814N <mail@thosearethegui.se>
2017-09-04 08:35:57 +1000

    Cleaned results of Stream 1

---
+++ BetterPasswordValidator/BetterPasswordValidator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// TODO:
+/// Better exception handling all round
+/// Call more api endpoints
+/// Create functions for the other password validation techniques
+/// 
+/// Provide useful feedback about how to improve the password
+/// Create function to perform a global check against all password validation tests
+/// </summary>
+
+
+namespace BetterPasswordValidator
+{
+    class BetterPasswordValidator
+    {
+        private readonly string BaseURL = @"https://haveibeenpwned.com/api/v2";
+
+        public BetterPasswordValidator()
+        {
+
+        }
+
+        /// <summary>
+        /// Takes a plain text password and forwards it on the the HaveIBeenPwned API
+        /// As a GET request
+        /// </summary>
+        /// <param name="password">Plain text password string</param>
+        /// <returns>Bool True if the password is known, False if the password safe*</returns>
+        public async Task<bool> HasPasswordBeenPwned(string password)
+        {
+            string endpoint = "pwnedpassword";
+
+            bool res = await GetRequestAsync(endpoint, password);
+
+            return res;
+
+        }
+
+        /// <summary>
+        /// Converts a plain text string to SHA1 hash before transit over a GET request
+        /// </summary>
+        /// <param name="password">Plain-text password string</param>
+        /// <returns>Bool True if the password is known, False if the password safe*</returns>
+        public async Task<bool> HasSha1PasswordBeenPwned(string password)
+        {
+            string endpoint = "pwnedpassword";
+
+            var sha1 = SHA1.Create();
+
+            var inputBytes = Encoding.ASCII.GetBytes(password);
+
+            var hash = sha1.ComputeHash(inputBytes);
+
+            string HashedPassword = BitConverter.ToString(hash).Replace("-", "");
+
+            bool res = await GetRequestAsync(endpoint, HashedPassword);
+
+            return res;
+
+        }
+
+        /// <summary>
+        /// Takes a plain text password and forwards it on the the HaveIBeenPwned API
+        /// As a POST request
+        /// </summary>
+        /// <param name="password">Plain text password string</param>
+        /// <returns>Bool True if the password is known, False if the password safe*</returns>
+        public async Task<bool> PostHasPasswordBeenPwned(string password)
+        {
+            string endpoint = "pwnedpassword";
+
+            KeyValuePair<string, string> Body = new KeyValuePair<string, string>("Password", password);
+
+            bool res = await PostRequestAsync(endpoint, Body);
+
+            return res;
+        }
+
+        /// <summary>
+        /// Generic GET request wrapper
+        /// </summary>
+        /// <param name="endpoint">The API Endpoint (service) to call</param>
+        /// <param name="parameter">The string parameter to pass to the endpoint</param>
+        /// <returns></returns>
+        private async Task<bool> GetRequestAsync(string endpoint, string parameter)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "007");
+            Uri api = new Uri($"{BaseURL}/{endpoint}/{parameter}");
+
+            var result = await client.GetAsync(api);
+            client.Dispose();
+
+            return (result.IsSuccessStatusCode ? true : false);
+
+        }
+
+        /// <summary>
+        /// Generic POST request wrapper
+        /// </summary>
+        /// <param name="endpoint">The API Endpoint (service) to call</param>
+        /// <param name="bosy">KeyValuePair for the POST Body</param>
+        /// <returns></returns>
+        private async Task<bool> PostRequestAsync(string endpoint, KeyValuePair<string, string> body)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "007");
+            Uri api = new Uri($"{BaseURL}/{endpoint}");
+
+            var RequestBody = new FormUrlEncodedContent(new[]
+            {
+                    body
+            });
+
+            var result = await client.PostAsync(api, RequestBody);
+            client.Dispose();
+            return (result.IsSuccessStatusCode ? true : false);
+
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# BetterPasswordValidator
+An all-in-one class library for use in validating and encouraging more secure passwords.
+
+The goal of this project is to create an all-in-one class library that can be used in applications to perform better password validation than the OOB alternative.
+
+The functions of this class library aim to help teach developers about the new [NIST Digital Identity Guidelines](https://pages.nist.gov/800-63-3/sp800-63b.html) as well as provide feedback to users encouraging them to think about the passwords they create and educate them in better password generation techniques.
+
+We will be streaming most of this projects development over on Twitch at https://www.twitch.tv/thosearetheguys/ come join us to hang out, help contribute to the solution or just bant about info sec, programming or general nerd stuff.
+
+## TODO
+* Lots at this stage
+* Possibly a built-in secure password generator
+
+## Credit
+Parts of the Class Library utilise the *Have I Been Pwned API v2* (https://haveibeenpwned.com/API/v2) to perform checks against previous account breaches and password leaks.
